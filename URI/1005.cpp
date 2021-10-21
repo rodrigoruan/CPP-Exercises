308b3b0bb34d9d47b4c6da58be5a3316f0f9fb14
rodrigoruan <rodrigopython16@gmail.com>
2021-10-21 10:59:41 -0300

    Added 1005 ex

---
+++ URI/1005.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  double a, b;
+  cin >> a;
+  cin >> b;
+  cout << "MEDIA = " << fixed << setprecision(5) << ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5) << endl;
+}
