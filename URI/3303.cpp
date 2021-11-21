a1cc7edbba53d14e0e0155053d08dc477bfdd235
Rodrigo <rodrigopython16@gmail.com>
2021-11-21 00:54:30 -0300

    added 3303

---
+++ URI/3303.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  string a;
+  cin >> a;
+
+  if(a.length() >= 10) {
+    cout << "palavrao" << endl;
+  } else {
+    cout << "palavrinha" << endl;
+  }
+}
