43bd0a369771d965058226e7d65890a5837469f5
Rodrigo <rodrigopython16@gmail.com>
2021-11-28 14:55:39 -0300

    bear and brother

---
+++ CodeForces/bearAndBrother.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int a, b, res=0;
+  cin >> a >> b;
+
+  while(a <= b) {
+    a *= 3;
+    b *= 2;
+    res++;
+  }
+
+  cout << res << "\n";
+}
