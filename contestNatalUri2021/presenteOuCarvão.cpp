ccc860a61e2cc04a01a7747e8db6b83eadc0050e
Rodrigo <rodrigopython16@gmail.com>
2021-12-29 15:44:13 -0300

    added presente ou carvao

---
+++ contestNatalUri2021/presenteOuCarvão.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n; cin >> n;
+  string res = n <= 25 ? "Recebe Presente, Feliz Natal!" : "Recebe Carvao";
+  cout << res << "\n";
+}
